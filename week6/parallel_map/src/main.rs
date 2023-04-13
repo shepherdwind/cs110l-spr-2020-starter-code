48eaf940da6fea55094d68497b0cee63afdce499
eward <eward.song@gmail.com>
2023-04-13 22:05:00 +0800

    refactor: Improve error handling and task management in parallel map computation.
    - Improve task management and error handling in parallel_map computation
    - Implement thread pool for faster task processing
    - Add time taken output for performance monitoring

--- week6/parallel_map/src/main.rs
+++ week6/parallel_map/src/main.rs
@@ -10,23 +10,46 @@ where
 {
     let mut output_vec: Vec<U> = Vec::new();
     let (s, r) = bounded(0);
+    let (s2, r2) = bounded(0);
     let total = input_vec.len();
+    let task_num = if total > num_threads { num_threads } else { total };
 
     let mut tasks = Vec::new();
-    for item in input_vec {
+    for i in 0..task_num {
         let sender = s.clone();
+        let receiver = r2.clone();
         let task = thread::spawn(move || {
-            println!("task {}: run", &item);
-            let ret = f(item);
-            println!("task {}: run end", item);
-            sender.send(ret).unwrap();
+            loop {
+                // when parent thread is stop to send, will drop sender
+                // then thread will resume, and get an error, thread should be stop
+                // after thread stoped, parent join can receiver message
+                let data = receiver.recv();
+                if data.is_err() {
+                    break;
+                }
+
+                println!("task {}: is start", i);
+                let ret = f(data.unwrap());
+                sender.send(ret).unwrap();
+            }
         });
         tasks.push(task);
     }
 
-    for _ in 0..total {
-        output_vec.push(r.recv().unwrap());
+    for _ in 0..task_num {
+        s2.send(input_vec.pop().unwrap()).unwrap();
     }
+
+    while output_vec.len() < total {
+        output_vec.push(r.recv().unwrap());
+        // if task is not finished, after recv data from channels,
+        // then send an task to thread pool
+        if let Some(item) = input_vec.pop() {
+            s2.send(item).unwrap();
+        }
+    }
+
+    drop(s2);
     for child in tasks {
         child.join().expect("oops! the child thread panicked");
     }
@@ -37,10 +60,12 @@ where
 fn main() {
     let v = vec![6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 12, 18, 11, 5, 20];
 
+    let start = time::Instant::now();
     let squares = parallel_map(v, 10, |num| {
         println!("{} squared is {}", num, num * num);
         thread::sleep(time::Duration::from_millis(500));
         num * num
     });
-    println!("squares: {:?}", squares);
+
+    println!("squares: {:?}, time: {:.2?}", squares, start.elapsed());
 }
