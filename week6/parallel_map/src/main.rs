b0b6724b719c161ff4c204dd41a9eef2d88b33d6
eward <eward.song@gmail.com>
2023-04-16 15:54:16 +0800

    chore: use threadpool for week6 task, much easy

--- week6/parallel_map/src/main.rs
+++ week6/parallel_map/src/main.rs
@@ -1,60 +1,28 @@
 
 use crossbeam_channel::bounded;
+use threadpool::ThreadPool;
 use std::{thread, time, fmt::Display};
 
-fn parallel_map<T, U, F>(mut input_vec: Vec<T>, num_threads: usize, f: F) -> Vec<U>
+fn parallel_map<T, U, F>(input_vec: Vec<T>, num_threads: usize, f: F) -> Vec<U>
 where
     F: FnOnce(T) -> U + Send + Copy + 'static,
     T: Send + Display + Copy + 'static,
     U: Send + 'static + Default,
 {
-    let mut output_vec: Vec<U> = Vec::new();
     let (s, r) = bounded(0);
-    let (s2, r2) = bounded(0);
-    let total = input_vec.len();
-    let task_num = if total > num_threads { num_threads } else { total };
+    let pool = ThreadPool::new(num_threads);
+    let len = input_vec.len();
 
-    let mut tasks = Vec::new();
-    for i in 0..task_num {
+    for item in input_vec {
         let sender = s.clone();
-        let receiver = r2.clone();
-        let task = thread::spawn(move || {
-            loop {
-                // when parent thread is stop to send, will drop sender
-                // then thread will resume, and get an error, thread should be stop
-                // after thread stoped, parent join can receiver message
-                let data = receiver.recv();
-                if data.is_err() {
-                    break;
-                }
-
-                println!("task {}: is start", i);
-                let ret = f(data.unwrap());
-                sender.send(ret).unwrap();
-            }
+        pool.execute(move || {
+            println!("task {}: is start", &item);
+            let ret = f(item);
+            sender.send(ret).unwrap();
         });
-        tasks.push(task);
     }
 
-    for _ in 0..task_num {
-        s2.send(input_vec.pop().unwrap()).unwrap();
-    }
-
-    while output_vec.len() < total {
-        output_vec.push(r.recv().unwrap());
-        // if task is not finished, after recv data from channels,
-        // then send an task to thread pool
-        if let Some(item) = input_vec.pop() {
-            s2.send(item).unwrap();
-        }
-    }
-
-    drop(s2);
-    for child in tasks {
-        child.join().expect("oops! the child thread panicked");
-    }
-
-    output_vec
+    r.iter().take(len).collect()
 }
 
 fn main() {
